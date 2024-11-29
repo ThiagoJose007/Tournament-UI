d65ff3ef8ea7c01ff301356908d23d4d9dca3765
Thiago José da Silva Santos <42985926+ThiagoJose007@users.noreply.github.com>
2024-11-28 23:10:18 -0300

    feat: ajustes
    Ajustes em redimensionamento, imagens e cenas

--- Tournament_UI/Assets/Art/UI/Components/Navbar/Navbar.cs
+++ Tournament_UI/Assets/Art/UI/Components/Navbar/Navbar.cs
@@ -4,9 +4,13 @@ using UnityEngine.UIElements;
 public class ApplyMaterialToUI : MonoBehaviour
 {
 
-    [System.Obsolete]
-    void Start()
+    private void OnEnable()
     {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var myButton = root.Q<Button>("Plus_button"); // Substitua pelo nome do seu botão
+        myButton.text = "Clique Aqui!";
+
+        var myButton1 = root.Q<Button>("Shadow_button"); // Substitua pelo nome do seu botão
+        myButton.text = "Clique Aqui!";
     }
-        
-}
+}   
